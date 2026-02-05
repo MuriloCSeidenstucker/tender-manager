48f07bbec1fccf3509195b3b2b0d8e8db4c8ca11
Murilo Seidenstucker <murilocampos.004@gmail.com>
2026-02-05 11:50:05 -0300

    feat: introduce database layer with SQLAlchemy (FastAPI lesson)

--- src/domain/models/__init__.py
+++ src/domain/models/__init__.py
@@ -1,4 +1,4 @@
 from src.domain.models.company import Company
 from src.domain.models.public_body import PublicBody
-from src.domain.models.tender import Tender
+from src.domain.models.tender import Tender, TenderFormat, TenderStatus, TenderType
 from src.domain.models.user import User
