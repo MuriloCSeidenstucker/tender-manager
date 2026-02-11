2128fda2da81b64a065a59dda9a9297a28b3a8a3
Murilo Seidenstucker <murilocampos.004@gmail.com>
2026-02-11 15:01:05 -0300

    feat: integrate SQLAlchemy session into user endpoints (FastAPI lesson)

--- src/app.py
+++ src/app.py
@@ -1,10 +1,16 @@
+# pylint: disable=R1720:no-else-raise
+
 from http import HTTPStatus
 
-from fastapi import FastAPI, HTTPException
+from fastapi import Depends, FastAPI, HTTPException
+from sqlalchemy import select
+from sqlalchemy.exc import IntegrityError
+from sqlalchemy.orm import Session
 
+from src.infra.entities.user_entity import UserEntity
+from src.infra.settings.database import get_session
 from src.presentation.schemas.user_schema import (
     Message,
-    UserDB,
     UserList,
     UserPublicSchema,
     UserSchema,
@@ -13,39 +19,76 @@ from src.presentation.schemas.user_schema import (
 app = FastAPI()
 
 
-database = []
-
-
 @app.post("/users/", status_code=HTTPStatus.CREATED, response_model=UserPublicSchema)
-def create_user(user: UserSchema):
-    user_with_id = UserDB(**user.model_dump(), id=len(database) + 1)
+def create_user(user: UserSchema, session: Session = Depends(get_session)):
+    db_user = session.scalar(
+        select(UserEntity).where(
+            (UserEntity.username == user.username) | (UserEntity.email == user.email)
+        )
+    )
 
-    database.append(user_with_id)
+    if db_user:
+        if db_user.username == user.username:
+            raise HTTPException(
+                status_code=HTTPStatus.CONFLICT,
+                detail="Username already exists",
+            )
+        elif db_user.email == user.email:
+            raise HTTPException(
+                status_code=HTTPStatus.CONFLICT,
+                detail="Email already exists",
+            )
 
-    return user_with_id
+    db_user = UserEntity(
+        username=user.username, password=user.password, email=user.email
+    )
+
+    session.add(db_user)
+    session.commit()
+    session.refresh(db_user)
+
+    return db_user
 
 
 @app.get("/users/", response_model=UserList)
-def read_users():
-    return {"users": database}
+def read_users(
+    skip: int = 0, limit: int = 100, session: Session = Depends(get_session)
+):
+    users = session.scalars(select(UserEntity).offset(skip).limit(limit)).all()
+    return {"users": users}
 
 
 @app.put("/users/{user_id}", response_model=UserPublicSchema)
-def update_user(user_id: int, user: UserSchema):
-    if user_id > len(database) or user_id < 1:
+def update_user(
+    user_id: int, user: UserSchema, session: Session = Depends(get_session)
+):
+    db_user = session.scalar(select(UserEntity).where(UserEntity.id == user_id))
+    if not db_user:
         raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail="User not found")
 
-    user_with_id = UserDB(**user.model_dump(), id=user_id)
-    database[user_id - 1] = user_with_id
+    try:
+        db_user.username = user.username
+        db_user.password = user.password
+        db_user.email = user.email
+        session.commit()
+        session.refresh(db_user)
 
-    return user_with_id
+        return db_user
+
+    except IntegrityError as e:
+        raise HTTPException(
+            status_code=HTTPStatus.CONFLICT, detail="Username or Email already exists"
+        ) from e
 
 
 @app.delete("/users/{user_id}", response_model=Message)
-def delete_user(user_id: int):
-    if user_id > len(database) or user_id < 1:
+def delete_user(user_id: int, session: Session = Depends(get_session)):
+    db_user = session.scalar(select(UserEntity).where(UserEntity.id == user_id))
+
+    if not db_user:
         raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail="User not found")
 
-    del database[user_id - 1]
+    session.delete(db_user)
+    session.commit()
 
     return {"message": "User deleted"}
