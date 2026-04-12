9a7a6b8f17a4e671e108839b3127d06fb591889a
Murilo Seidenstucker <murilocampos.004@gmail.com>
2026-04-12 11:52:39 -0300

    feat: add CORS middleware for frontend integration

--- src/app.py
+++ src/app.py
@@ -3,14 +3,23 @@ import sys
 from http import HTTPStatus
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from src.routers import auth, companies, tenders, users
-from src.schemas import MessageSchema
+from src.schemas.common import MessageSchema
 
 if sys.platform == "win32":
     asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(users.router)
 app.include_router(auth.router)
